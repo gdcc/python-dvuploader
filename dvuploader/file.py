f6a7528a1e8965cb0cf9b210828f4d3b8d3428a9
Jan Range <30547301+JR-1991@users.noreply.github.com>
2023-10-20 10:35:10 +0200

    Added file check

--- dvuploader/file.py
+++ dvuploader/file.py
@@ -1,7 +1,7 @@
 import os
 from typing import List, Optional
 
-from pydantic import BaseModel, Field, validator
+from pydantic import BaseModel, Field, validator, ValidationError
 
 from dvuploader.checksum import Checksum, ChecksumTypes
 
@@ -21,12 +21,25 @@ class File(BaseModel):
     fileName: Optional[str] = None
     checksum: Optional[Checksum] = None
 
+    @staticmethod
+    def _validate_filepath(path):
+        if not os.path.exists(path):
+            raise FileNotFoundError(f"Filepath {path} does not exist.")
+        elif not os.path.isfile(path):
+            raise TypeError(f"Filepath {path} is not a file.")
+        elif not os.access(path, os.R_OK):
+            raise TypeError(f"Filepath {path} is not readable.")
+        elif os.path.getsize(path) == 0:
+            raise ValueError(f"Filepath {path} is empty.")
+        return path
+
     @validator("fileName", always=True)
     def _extract_filename(cls, v, values):
         return os.path.basename(values["filepath"])
 
     @validator("checksum", always=True)
     def _calculate_hash(cls, v, values):
+        cls._validate_filepath(values["filepath"])
         fpath = values["filepath"]
         hash_algo, hash_fun = values["checksum_type"].value
 
