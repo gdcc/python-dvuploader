84e8f2b8e4aec003d079ad9d7130a63e352ffeb9
Jan Range <range.jan@web.de>
2024-01-09 18:31:04 +0100

    migrate to `pydantic` v2

--- dvuploader/file.py
+++ dvuploader/file.py
@@ -1,47 +1,82 @@
 import os
 from typing import List, Optional
 
-from pydantic import BaseModel, Field, validator
+from pydantic import BaseModel, Field, model_validator
 
 from dvuploader.checksum import Checksum, ChecksumTypes
 
 
 class File(BaseModel):
+    """
+    Represents a file with its properties and methods.
+
+    Attributes:
+        filepath (str): The path to the file.
+        description (str): The description of the file.
+        directoryLabel (str): The label of the directory where the file is stored.
+        mimeType (str): The MIME type of the file.
+        categories (List[str]): The categories associated with the file.
+        restrict (bool): Indicates if the file is restricted.
+        checksum_type (ChecksumTypes): The type of checksum used for the file.
+        storageIdentifier (Optional[str]): The identifier of the storage where the file is stored.
+        fileName (Optional[str]): The name of the file.
+        checksum (Optional[Checksum]): The checksum of the file.
+        to_replace (bool): Indicates if the file should be replaced.
+        file_id (Optional[str]): The ID of the file.
+
+    Methods:
+        _validate_filepath(path): Validates if the file path exists and is a file.
+        _extract_filename_hash_file(): Extracts the filename from the filepath and calculates the file's checksum.
+
+    """
+
     filepath: str = Field(..., exclude=True)
     description: str = ""
     directoryLabel: str = ""
     mimeType: str = "text/plain"
     categories: List[str] = ["DATA"]
     restrict: bool = False
-    checksum_type: ChecksumTypes = Field(
-        default=ChecksumTypes.MD5,
-        exclude=True,
-    )
+    checksum_type: ChecksumTypes = Field(default=ChecksumTypes.MD5, exclude=True)
     storageIdentifier: Optional[str] = None
     fileName: Optional[str] = None
     checksum: Optional[Checksum] = None
     to_replace: bool = False
     file_id: Optional[str] = None
 
+    @model_validator(mode="after")
+    def _extract_filename_hash_file(self):
+        """
+        Extracts the filename and calculates the hash of the file.
+
+        Returns:
+            self: The current instance of the class.
+        """
+        self._validate_filepath(self.filepath)
+        self.fileName = os.path.basename(self.filepath)
+
+        # Hash file
+        hash_algo, hash_fun = self.checksum_type.value
+        self.checksum = Checksum.from_file(
+            fpath=self.filepath,
+            hash_fun=hash_fun,
+            hash_algo=hash_algo,
+        )
+
+        return self
+
     @staticmethod
     def _validate_filepath(path):
+        """
+        Validates if the given filepath exists and is a file.
+
+        Args:
+            path (str): The filepath to be validated.
+
+        Raises:
+            FileNotFoundError: If the filepath does not exist.
+            TypeError: If the filepath is not a file.
+        """
         if not os.path.exists(path):
             raise FileNotFoundError(f"Filepath {path} does not exist.")
         elif not os.path.isfile(path):
-            raise TypeError(f"Filepath {path} is not a file.")
-        elif not os.access(path, os.R_OK):
-            raise TypeError(f"Filepath {path} is not readable.")
-
-        return path
-
-    @validator("fileName", always=True)
-    def _extract_filename(cls, v, values):
-        return os.path.basename(values["filepath"])
-
-    @validator("checksum", always=True)
-    def _calculate_hash(cls, v, values):
-        cls._validate_filepath(values["filepath"])
-        fpath = values["filepath"]
-        hash_algo, hash_fun = values["checksum_type"].value
-
-        return Checksum.from_file(fpath=fpath, hash_fun=hash_fun, hash_algo=hash_algo)
+            raise IsADirectoryError(f"Filepath {path} is not a file.")
