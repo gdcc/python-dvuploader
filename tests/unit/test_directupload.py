4bf11236db1791ba30cb339035f01fef96d52e8c
Jan Range <range.jan@web.de>
2024-02-29 10:48:52 +0100

    adapt tests to new function

--- tests/unit/test_directupload.py
+++ tests/unit/test_directupload.py
@@ -3,9 +3,9 @@ import aiohttp
 import pytest
 from rich.progress import Progress
 from dvuploader.directupload import (
-    _add_file_to_ds,
     UPLOAD_ENDPOINT,
     REPLACE_ENDPOINT,
+    _add_files_to_ds,
     _validate_ticket_response,
 )
 
@@ -25,16 +25,16 @@ class Test_AddFileToDs:
         dataverse_url = "https://example.com"
         pid = "persistent_id"
         fpath = "tests/fixtures/add_dir_files/somefile.txt"
-        file = File(filepath=fpath)
+        files = [File(filepath=fpath)]
         progress = Progress()
         pbar = progress.add_task("Uploading", total=1)
 
         # Invoke the function
-        result = await _add_file_to_ds(
+        await _add_files_to_ds(
             session=session,
             dataverse_url=dataverse_url,
             pid=pid,
-            file=file,
+            files=files,
             progress=progress,
             pbar=pbar,
         )
@@ -55,23 +55,23 @@ class Test_AddFileToDs:
         dataverse_url = "https://example.com"
         pid = "persistent_id"
         fpath = "tests/fixtures/add_dir_files/somefile.txt"
-        file = File(filepath=fpath, file_id="0")
+        files = [File(filepath=fpath, file_id="0")]
         progress = Progress()
         pbar = progress.add_task("Uploading", total=1)
 
         # Invoke the function
-        result = await _add_file_to_ds(
+        await _add_files_to_ds(
             session=session,
             dataverse_url=dataverse_url,
             pid=pid,
-            file=file,
+            files=files,
             progress=progress,
             pbar=pbar,
         )
 
         # Assert that the response status is 200 and the result is True
         assert mock_post.called_with(
-            urljoin(dataverse_url, REPLACE_ENDPOINT.format(FILE_ID=file.file_id)),
+            urljoin(dataverse_url, REPLACE_ENDPOINT + pid),
             data=mocker.ANY,
         )
 
