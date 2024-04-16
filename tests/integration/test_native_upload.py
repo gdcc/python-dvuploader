0e57023ebc779e67009d369ac18037dc2ef3db87
Jan Range <range.jan@web.de>
2024-04-16 20:21:05 +0200

    add handler test case

--- tests/integration/test_native_upload.py
+++ tests/integration/test_native_upload.py
@@ -1,7 +1,9 @@
+from io import BytesIO
 import tempfile
 
 import pytest
 from dvuploader.dvuploader import DVUploader
+from dvuploader.file import File
 
 from dvuploader.utils import add_directory, retrieve_dataset_files
 from tests.conftest import create_dataset, create_mock_file
@@ -100,3 +102,47 @@ class TestNativeUpload:
 
             assert len(files) == 3
             assert sorted([file["label"] for file in files]) == sorted(expected_files)
+
+
+    def test_native_upload_by_handler(
+        self,
+        credentials,
+    ):
+        BASE_URL, API_TOKEN = credentials
+
+        # Arrange
+        byte_string = b"Hello, World!"
+        files = [
+            File(filepath="subdir/file.txt", handler=BytesIO(byte_string)),
+            File(filepath="biggerfile.txt", handler=BytesIO(byte_string*10000)),
+        ]
+
+        # Create Dataset
+        pid = create_dataset(
+            parent="Root",
+            server_url=BASE_URL,
+            api_token=API_TOKEN,
+        )
+
+        # Act
+        uploader = DVUploader(files=files)
+        uploader.upload(
+            persistent_id=pid,
+            api_token=API_TOKEN,
+            dataverse_url=BASE_URL,
+            n_parallel_uploads=1,
+        )
+
+        # Assert
+        expected_files = [
+            "file.txt",
+            "biggerfile.txt",
+        ]
+        files = retrieve_dataset_files(
+            dataverse_url=BASE_URL,
+            persistent_id=pid,
+            api_token=API_TOKEN,
+        )
+
+        assert len(files) == 2
+        assert sorted([file["label"] for file in files]) == sorted(expected_files)
